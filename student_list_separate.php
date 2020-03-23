850d29f1bf8716bf2b0e748703cdd4004820d714
RioAdiNugraha <rioadinugraha@yahoo.com>
2020-03-23 23:19:06 +0700

    UI edits

--- student_list_separate.php
+++ student_list_separate.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+</head>
+<body>
+
 <?php
 include "include/header.php";
 ?>
@@ -5,11 +14,11 @@ include "include/header.php";
 
 	<h1>Student List</h1>
 
-	<a href="student_manage.php">
+	<!-- <a href="student_manage.php">
 		+ New Student
-	</a>
+	</a> -->
 
-	<table>
+	<table class="table table-striped table-dark">
 		<tr>
 			<th>Student ID</th>
 			<th>Student Name</th>
@@ -18,10 +27,11 @@ include "include/header.php";
 		<tr ng-repeat="student in students">
 			<td>{{ student.Studentid }}</td>
 			<td>{{ student.Name }}</td>
-			<td><input type="button" value="Delete" id={{student.Studentid}} ng-click="delete($event)"></td>
-			<td><input type="button" value="Update" id={{student.StudentId}} ng-click="update()"></td>
+			<td><input type="button" value="Delete" id={{student.Studentid}} class="btn btn-primary btn-lg btn-block"ng-click="delete($event)"></td>
+			<td><input type="button" value="Update" id={{student.StudentId}} class="btn btn-info btn-lg btn-block"ng-click="update()"></td>
 		</tr>
 	</table>
+	<a class="btn btn-primary" href="student_manage.php" role="button">Add New Student</a>
 
 </div>
 
@@ -36,13 +46,13 @@ app.controller('studentCtrl', function($scope, $http) {
    $scope.delete = function(event){
 		var studentid = event.target.id;
 		console.log(studentid);
-		
+
 		$http.delete("student_crud.php?studentid=" + studentid)
 			.then(function (response) {
 					console.log(response);
 					$scope.refresh();
 				})
-			.catch(function (err) { 
+			.catch(function (err) {
 					console.log("error while requesting to delete data to server" + err)
 				});
    };
@@ -55,3 +65,13 @@ app.controller('studentCtrl', function($scope, $http) {
 <?php
 // include "include/footer.php";
 ?>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+
+
+
+</html>
--- student_manage.php
+++ student_manage.php
@@ -1,79 +1,124 @@
-<?php
-include "include/conn_db.php";
-include "include/header.php";
-// include "include/student_crud_01_insert.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-// if($_REQUEST['submitbtn'] == 'Save'){
-//     student_insert($_REQUEST['studentid'], $_REQUEST['name'], $_REQUEST['password']);
-//     $message = 'Data Saved';
-// }
-?>
 
-<h1>Manage Student Data</h1>
-<?php
-    // echo $message;
-?>
+</head>
+<body>
 
-<div ng-app="myApp" ng-controller="studentCtrl">
-    <h3 ng-value="message"></h3>
-	<form ng-submit="submit()">
-		<label for="studentid">Student ID</label>
-		<input type="text" id="studentid" ng-model="studentid">
+  <?php
+  include "include/conn_db.php";
+  include "include/header.php";
+  // include "include/student_crud_01_insert.php";
 
-		<label for="studentname">Student Name</label>
-		<input type="text" id="studentname" ng-model="studentname">
+  // if($_REQUEST['submitbtn'] == 'Save'){
+  //     student_insert($_REQUEST['studentid'], $_REQUEST['name'], $_REQUEST['password']);
+  //     $message = 'Data Saved';
+  // }
+  ?>
 
-        <label for="studentname">Password</label>
-		<input type="password" id="studentname" ng-model="studentpassword">
+  <h1>Manage Student Data</h1>
+  <?php
+      // echo $message;
+  ?>
+  <div ng-app="myApp" ng-controller="studentCtrl">
+  <form ng-submit="submit()">
+      <div class="form-group row">
+        <label for="studentid" class="col-sm-2 col-form-label">Student ID</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="studentid" name="studentid" placeholder="Student ID" ng-model="studentid">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="studentname" class="col-sm-2 col-form-label">Name</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="studentname" name="studentname" placeholder="Student Name" ng-model="studentname">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="studentpassword" class="col-sm-2 col-form-label">Password</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="studentpassword" name="studentpassword" placeholder="Password" ng-model="studentpassword">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="offset-sm-2 col-sm-10">
+          <input type="submit" value="submit" class="btn btn-primary btn-lg"/>
+        </div>
+      </div>
+    </form>
+  </div>
 
-		<input type="submit"  value="submit">
-	</form>
-</div>
-<!-- <form method="post">
-    <input type="text" name="studentid" placeholder="Student ID">
-    <br><br>
+<a class="btn btn-secondary" href="student_list_separate.php" role="button">Return to Homepage</a>
 
-    <input type="text" name="name" placeholder="Student Name">
-    <br><br>
+  <!-- <div ng-app="myApp" ng-controller="studentCtrl">
+      <h3 ng-value="message"></h3>
+  	<form ng-submit="submit()">
+  		<label for="studentid">Student ID</label>
+  		<input type="text" id="studentid" ng-model="studentid">
 
-    <input type="password" name="password" placeholder="Password">
-    <br><br>
+  		<label for="studentname">Student Name</label>
+  		<input type="text" id="studentname" ng-model="studentname">
 
-    <input type="submit" name="submitbtn" value="Save">
-    <input type="reset" value="Reset">
-</form> -->
+          <label for="studentname">Password</label>
+  		<input type="password" id="password" ng-model="studentpassword">
 
-<br>
+  		<input type="submit"  value="submit">
+  	</form>
+  </div> -->
+  <!-- <form method="post">
+      <input type="text" name="studentid" placeholder="Student ID">
+      <br><br>
 
-<script>
-var app = angular.module('myApp', []);
-app.controller('studentCtrl', function($scope, $http) {
+      <input type="text" name="name" placeholder="Student Name">
+      <br><br>
 
-   $scope.submit = function(event){
-        var data = {
-			studentid : $scope.studentid,
-            name : $scope.studentname,
-            password: $scope.studentpassword
-		};
+      <input type="password" name="password" placeholder="Password">
+      <br><br>
 
-        console.log(data);
-        $http.post("student_crud.php",JSON.stringify(data))
-			.then(function (response) {
-                    console.log("data added successfully");
-                    alert("data added successfully");
-                    // $scope.message = "data added successfully";
-				})
-			.catch(function (err) { 
-					console.log("error while requesting to insert data to server" + err)
-				});
-   }
+      <input type="submit" name="submitbtn" value="Save">
+      <input type="reset" value="Reset">
+  </form> -->
 
-});
+  <br>
+
+  <script>
+  var app = angular.module('myApp', []);
+  app.controller('studentCtrl', function($scope, $http) {
+
+     $scope.submit = function(event){
+          var data = {
+  			studentid : $scope.studentid,
+              name : $scope.studentname,
+              password: $scope.studentpassword
+  		};
+
+
+
+          console.log(data);
+          $http.post("student_crud.php",JSON.stringify(data))
+  			.then(function (response) {
+                      console.log("data added successfully");
+                      alert("data added successfully");
+                      // $scope.message = "data added successfully";
+  				})
+  			.catch(function (err) {
+  					console.log("error while requesting to insert data to server" + err)
+  				});
+     }
+
+  });
+
 
 
 </script>
+ <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
 
-<?php
-// include "include/footer.php";
-?>
 
+
+
+</html>
